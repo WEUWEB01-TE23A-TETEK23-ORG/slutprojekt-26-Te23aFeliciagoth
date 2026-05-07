35f19f25129d83ddfe2b83a7ff1ab0a791f48cc0
Felicia Göth <Felicia.goth@elev.ga.ntig.se>
2026-05-07 10:57:54 +0200

    snart klar

---
+++ rymd.js
@@ -0,0 +1,10 @@
+
+
+function visaVideo() {
+    document.querySelector(".video").style.display = "block"
+    }
+
+
+function merText2() {
+    document.querySelector(".mertext").style.display = "block"
+}
